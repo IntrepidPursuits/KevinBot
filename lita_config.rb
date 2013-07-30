6676f2ff35dedc126257f15a8dbbb2be95e7a0ac
Edward Loveall <edward@edwardloveall.com>
2013-07-30 18:47:19 -0400

    Initial commit

---
+++ lita_config.rb
@@ -0,0 +1,12 @@
+# Email: edward+kb@edwardloveall.com
+# Pass: kevinbot
+
+Lita.configure do |config|
+  config.robot.name = "Kevin Bot"
+  config.robot.adapter = :hipchat
+  config.adapter.jid = "59616_409420@chat.hipchat.com"
+  config.adapter.password = "kevinbot"
+  config.adapter.debug = true
+  config.adapter.rooms = :all
+  config.adapter.muc_domain = "conf.hipchat.com"
+end
