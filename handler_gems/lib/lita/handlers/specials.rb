4174109b46d4f7cc92809213894a9307779732cd
Edward Loveall <edward@edwardloveall.com>
2013-08-01 09:31:31 -0400

    Added Squeaky Beaker specials

--- handler_gems/lib/lita/handlers/specials.rb
+++ handler_gems/lib/lita/handlers/specials.rb
@@ -7,17 +7,36 @@ module Lita
       route (/^specials?/), :list_specials, command: true, help: {
         "specials" => "Lists the food specials"
       }
-      
-      def list_specials(response)
-        secondStDoc = Nokogiri::HTML(open('http://www.2ndstcafe.com/'))
-        secondStreetSpecials = secondStDoc.search('#entree-specials ul li').map { |x| x.content }
-        secondStreetSpecials.unshift("***2nd Street***")
 
-        specials = secondStreetSpecials
-        
-        response.reply('These are the specials:', specials)
+      def list_specials(response)
+        specials = get_2nd_street
+        specials += get_squeaky_beaker
+
+        response.reply('These are the specials:', specials.join("\n"))
+      end
+      Lita.register_handler(Hello)
+
+      def get_2nd_street
+        secondStDoc = Nokogiri::HTML(open('http://www.2ndstcafe.com/'))
+        specials = secondStDoc.search('#entree-specials ul li').map { |e| e.content }
+        specials.unshift("***2nd Street***")
+      end
+
+      def get_squeaky_beaker
+        squeakyDoc = Nokogiri::HTML(open('http://www.squeakybeaker.com/'))
+        specials = squeakyDoc.search('div.entry_content p')
+
+        soupIndex = nil;
+        specials.each_with_index do |e, i|
+          if e.content.match('Soups:')
+            soupIndex = i
+          end
+        end
+
+        specials[soupIndex].content = "***Squeaky Beaker Soups***"
+        specials = specials.map { |e| e.content }
+        specials.unshift("***Squeaky Beaker Specials***")
       end
     end
-    Lita.register_handler(Hello)
   end
 end
