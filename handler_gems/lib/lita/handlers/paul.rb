bd9700170609221f958feb58a0fefa865f8c0260
Edward Loveall <edward@edwardloveall.com>
2013-08-05 18:28:46 -0400

    Added Paul handler

---
+++ handler_gems/lib/lita/handlers/paul.rb
@@ -0,0 +1,17 @@
+require "nokogiri"
+require "open-uri"
+
+module Lita
+  module Handlers
+    class Paul < Handler
+      route (/.?/), :paul
+
+      def paul(response)
+        if rand < 0.001 # One in every 1000
+          response.reply("shut up paul")
+        end
+      end
+      Lita.register_handler(Paul)
+    end
+  end
+end
