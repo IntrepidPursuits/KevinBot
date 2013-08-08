88460d33c9e848c857171b4630ce0d233bbefa04
Edward Loveall <edward@edwardloveall.com>
2013-08-08 17:01:29 -0400

    God damn it

--- handler_gems/lib/lita/handlers/paul.rb
+++ handler_gems/lib/lita/handlers/paul.rb
@@ -6,11 +6,10 @@ module Lita
       def paul(response)
         if rand < (1/1000.0)
           response.reply("shut up paul")
-        else
-          response.reply(rand)
         end
       end
       Lita.register_handler(Paul)
+
     end
   end
 end
