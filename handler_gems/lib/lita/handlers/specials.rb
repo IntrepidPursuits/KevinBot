381b56b765e4129a81f4366d65fefaa52cb04568
Edward Loveall <edward@edwardloveall.com>
2014-08-29 16:08:58 -0400

    Refactor specials to be less crappy

--- handler_gems/lib/lita/handlers/specials.rb
+++ handler_gems/lib/lita/handlers/specials.rb
@@ -9,53 +9,55 @@ module Lita
       }
 
       def list_specials(response)
-        specials = get_2nd_street
-        specials.push('')
-        specials += get_squeaky_beaker
+        specials = []
+        specials << get_2nd_street
+        specials << ''
+        specials << get_squeaky_beaker
 
         response.reply(specials.join("\n"))
       end
       Lita.register_handler(Specials)
 
       def get_2nd_street
-        separator = "=========================="
-        secondStDoc = Nokogiri::HTML(open('http://www.2ndstcafe.com/'))
+        page = Nokogiri::HTML(open('http://www.2ndstcafe.com/'))
 
         specials = Array.new
         specials.push("2ND STREET")
-        specials.push(separator)
 
-        dateDom = secondStDoc.search('div#block-yui_3_10_1_1_1398362757307_5306 h1')
-        specials.push(dateDom.children.first.content.upcase)
+        food = page.search('#block-yui_3_10_1_1_1398362757307_5306')
+        date = food.search('h1')
+        specials.push(date.children.text)
 
-        foodDom = secondStDoc.search('div#block-yui_3_10_1_1_1398362757307_5306 ul li, div#block-yui_3_10_1_1_1398362757307_5306 p')
-        soupDom = secondStDoc.search('div#block-yui_3_10_1_1_1400159060403_5119 ul li, div#block-yui_3_10_1_1_1400159060403_5119 p')
-        specials.push(get_group_content(foodDom))
+        meals = food.search('p')
+        specials.push(get_group_content(meals))
 
-        specials.push("2ND STREET SOUPS")
-        specials.push(get_group_content(soupDom))
+        soup = page.search('#block-yui_3_17_2_1_1409338497528_7786 ul li')
+        specials.push(get_group_content(soup)).flatten
       end
 
       def get_squeaky_beaker
-        separator = "=========================="
-        squeakyDoc = Nokogiri::HTML(open('http://www.squeakybeaker.com/'))
-
-        specialsDOM = squeakyDoc.search('div.entry_content h4')
-        soupsDOM = squeakyDoc.search('div.entry_content p')
+        page = Nokogiri::HTML(open('http://www.squeakybeaker.com/'))
 
         specials = Array.new
-        specials.push("SQUEAKY BEAKER")
-        specials.push(separator)
-        specials.push(specialsDOM[specialsDOM.length - 1].content)
+        food_soups = page.search('div.entry_content p')
 
-        specials.push("SQUEAKY BEAKER SOUPS")
-        soupsDOM.shift
-        specials.push(soupsDOM.map { |e| e.content })
+        specials.push("SQUEAKY BEAKER")
+        specials << food_soups.map(&:text).reject! { |ele|
+          /(- \d+\.\d+ -|Soup)/ === ele
+        }
+        specials.flatten
       end
 
       def get_group_content(group)
-        group.children.map { |ele| ele.content }
+        content = group.children.map { |ele| ele.text }
+        content.reject { |text| text.blank? }.compact.flatten
       end
     end
+  end
+end
+
+class String
+  def blank?
+    /\A[[:space:]]*\z/ === self
   end
 end
